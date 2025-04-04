323537c8ff7f30ea59abe81102b4cbfd8afe6453
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2025-04-03 23:21:42 -0400

    Some Initial Cleanup

---
+++ react-community-app/src/components/header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className ="">
+                <h1>Welcome To The Forum App!</h1>
+        </header>
+    );
+};
---
+++ react-community-app/src/components/post.jsx
@@ -0,0 +1,15 @@
+export const Post = ({id, title}) => {
+    return (
+        <div className ="post-card">
+            <div className="post-card-body">
+                <div className="post-title"><label className=""><b><u>{title}</u></b></label></div>
+                <p className="post-description">
+
+                </p>
+                <p className="interaction-box">
+
+                </p>
+            </div>
+        </div>
+    );
+};
